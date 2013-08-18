af5c386bfcc9f5876c1701a9f0ebd37aa31cf730
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2013-08-18 11:13:28 +0200

    up to 49 digit

--- Numbers/Pi/pi.py
+++ Numbers/Pi/pi.py
@@ -11,11 +11,11 @@ def pi():
 	return 2*opi(1)
 
 def opi(i):
-	if i==50:
+	if i==800:
 		return 1
 	else:
 		n = 1.0 + i / (2.0 * i + 1) * opi(i + 1)
 		return n
 
 if __name__=="__main__":
-	print pi()
+	print 'the number is {:.49}'.format(pi())
