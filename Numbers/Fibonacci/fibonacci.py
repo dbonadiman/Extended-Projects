3024eda0040872b178326c6b38c2b4dd7b6d63dc
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2013-08-19 11:03:27 +0200

    fibonacci iterative added.

--- Numbers/Fibonacci/fibonacci.py
+++ Numbers/Fibonacci/fibonacci.py
@@ -1,11 +1,28 @@
-def fibonacci(n):
-	fib(0,1,n)
+###########################
+# recursive one suffers of
+# recursion limit exceeded on 
+# large number.
+##########################
+def fibonacci():
+	fib()
 	
 
-def fib(a,b,n):
+def fib():
+	a=0
+	b=1
+	while n>b:
+		print b
+		c=b
+		b=a+b
+		a=c
+
+
+
+def fib_rec(a,b):
 	if n>b:
 		print b
-		fib(b,a+b,n)
+		fib(b,a+b)
 		
 if __name__=="__main__":
-	fibonacci(int(input("Give me a number:")))
+	n = int(input("Give me a number:"))
+	fibonacci()
