7a2d04e561858ce91526a03efaf16bf1e67d6291
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2013-08-20 15:55:14 +0200

    collar code optimised

--- Classic Algorithms/Collaz Conjecture/collaz_conjecture.py
+++ Classic Algorithms/Collaz Conjecture/collaz_conjecture.py
@@ -4,21 +4,20 @@ import sys
 def collaz(num):
 	steps = 0
 	while True:
+		if num==1:
+			break
 		steps += 1
 		if num%2==0:
 			num=num/2
 		else:
-			num=num*3+1
-		if num==1:
-			break
+			num=num*3+1		
 	return steps
 
 
+def main(argv):
+	num = int(argv[1])
+	print collaz(num) 
 
 
 if __name__ == "__main__":
-	num = int(sys.argv[1])
-	if num>1:
-		print collaz(num)
-	else:
-		print 'already one'  
+	main(sys.argv)
