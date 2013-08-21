8732577bc53c2c06b245807984135bbc5675b2b1
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2013-08-21 11:48:19 +0200

    binary to decimal and back done:)
    Used string for binary

---
+++ Numbers/BinaryToDecimal/binarytodecimal.py
@@ -0,0 +1,25 @@
+def decimal_to_binary(num):
+	binary = ""
+	while num>0:
+		binary+=str(num%2)
+		num = num/2
+	return binary[::-1] #this reverse the string
+
+
+def binary_to_decimal(bin):
+	decimal = 0
+	for i in range(0,len(bin)):
+		if int(bin[i])==1:
+			decimal += (2)**((len(bin)-1)-i)
+	return decimal
+
+
+def main():
+	decimal = int(raw_input("A decimal number: "))
+	print decimal_to_binary(decimal)
+	binary = str(raw_input("A binary number: "))
+	print binary_to_decimal(binary)
+
+
+if __name__=="__main__":
+	main()
