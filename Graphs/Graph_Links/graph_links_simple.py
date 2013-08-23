538fdb68ea9ca9650606ec95a71cd7477dec4bc6
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2013-08-23 11:41:35 +0200

    Graph links solution using matrix instead of Class to represent graphs

---
+++ Graphs/Graph_Links/graph_links_simple.py
@@ -0,0 +1,22 @@
+def __init_graph__(maximum):
+	return [[0 for j in range(0,maximum+1)] for i in range(0,maximum+1)]
+
+
+
+def graph(links):	
+	m =  max(max(n for (n,k) in links),max(k for (n,k) in links))
+	graph = __init_graph__(m)
+	for (n,k) in links:
+		graph[n][k] = 1
+	return graph
+
+def print_matrix(m):
+	for l in m:
+		print l
+
+def main():
+	links = [(1,2),(2,3),(5,6),(7,8)]
+	print_matrix(graph(links))
+
+if __name__=="__main__":
+	main()
