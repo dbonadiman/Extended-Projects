59634695d9ece2a51469d1972a589f5d2098e897
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2013-08-26 01:31:17 +0200

    /Users/dbonadiman/Development/tantaroba/Files/Zip File Maker/divina_commedia.txt /Users/dbonadiman/Development/tantaroba/Files/Zip File Maker/huffman.py
    now huffman compress folders

--- Files/Zip File Maker/huffman.py
+++ Files/Zip File Maker/huffman.py
@@ -103,11 +103,11 @@ class Dx:
 		
 
 
-	def compress(self):
+	def __compress_file(self):
 		string = ""
-		f = open(self.fi, 'r')
+		f = open(self.fi, 'rb')
 		while True:
-			s = f.readline()
+			s = f.read(1)
 			if s=='':
 				break
 			string += s
@@ -116,10 +116,10 @@ class Dx:
 		zip = self.__huffman_enc(string)
 		if not os.path.exists(name+'.temp'):
 			os.makedirs(name+'.temp')
-		f = open(name+'.temp/'+name+'.tx','wb')
+		f = open(name+'.temp/'+name.split('/')[-1]+'.tx','wb')
 		f.write(zip[0])
 		f.close()
-		cPickle.dump(zip[1],open(name+'.temp/'+name+'.ht','wb'))
+		cPickle.dump(zip[1],open(name+'.temp/'+name.split('/')[-1]+'.ht','wb'))
 		files=os.listdir(name+'.temp')
 		out =open(self.fi+'.dx', 'wb')
 		for f in files:
@@ -138,7 +138,7 @@ class Dx:
 
 
 
-	def decompress(self):
+	def __decompress_file(self):
 		f = open(self.fi,'rb')
 		
 		size = []
@@ -149,19 +149,18 @@ class Dx:
 				break
 			size += [int(a.replace('\n',''))]
 		name  = self.fi[:len(self.fi)-3]
-		print name
 		data = f.read()
 		data = data.replace(header,'')
 		f.close()
 		if not os.path.exists(name+'.temp'):
 			os.makedirs(name+'.temp')
-		f = open(name+'.temp/'+name+'.ht','wb')
+		f = open(name+'.temp/'+name.split('/')[-1]+'.ht','wb')
 		f.write(data[:size[0]])
 		f.close()
 			
 		aa = data[size[0]:]
 		f.close()
-		string = self.__huffman_dec((aa,cPickle.load(open(name+'.temp/'+name+'.ht','rb'))))
+		string = self.__huffman_dec((aa,cPickle.load(open(name+'.temp/'+name.split('/')[-1]+'.ht','rb'))))
 
 		f = open(name,'wb')
 		f.write(string+'\n')
@@ -169,15 +168,88 @@ class Dx:
 		shutil.rmtree(name+'.temp')
 		return name
 
+	def compress(self):
+		if not os.path.isfile(self.fi):
+			f = open(self.fi+"_c",'wb')
+			info = open(self.fi+".f",'wb')
+			files=os.listdir(self.fi)
+			info.write('<<FOLDER>>\n')
+			for fa in files:
+				o = Dx(self.fi+"/"+fa).compress()
+				a = open(o,'rb')
+				info.write(o+'\t'+str(os.path.getsize(o))+'\n')
+				os.remove(o)
+				f.write(a.read())
+				a.close()
+			info.write('\n')
+			f.close()
+			f = open(self.fi+"_c",'rb')
+			info.write(f.read())
 
+			f.close()
+			info.close()
+			out = Dx(self.fi+".f").compress()
+			os.remove(self.fi+"_c")
+			os.remove(self.fi+".f")
+			#os.rename(out,out.replace('.f',''))
+			return out
+		else: 
+			return Dx(self.fi).__compress_file()
+
+	def decompress(self):
+		if self.fi[-2:]!='dx':
+			raise Exception("File not compressed")
+		o = Dx(self.fi).__decompress_file()
+		info = open(o,'rb')
+		if info.readline()=='<<FOLDER>>\n':
+			files = []
+			while True:
+				s = info.readline()
+				if s=='\n':
+					break
+				a = s.split('\t')
+				files += [(a[0],int(a[1].replace('\n','')))]
+			data = info.read()
+			if not os.path.exists(o[:-2]):
+				os.makedirs(o[:-2])
+			t = 0
+			print files
+			for f in files:
+				print f
+				fa = open(f[0],'wb')
+				fa.write(data[t:t+f[1]])
+				fa.close()
+				t += f[1]
+				Dx(f[0]).decompress()
+				os.remove(f[0])
+			os.remove(o)
+			o = o[:-2]
+		return o
+
+
+
+
+
+
+
+def get_size(start_path = '.'):
+	if os.path.isfile(start_path):
+		return os.path.getsize(start_path)
+	total_size = 0
+	for dirpath, dirnames, filenames in os.walk(start_path):
+		for f in filenames:
+			fp = os.path.join(dirpath, f)
+			total_size += os.path.getsize(fp)
+	return total_size
 	
 def main(op,fi):
+	o = ''
 	if op=='-c':
 		o = Dx(fi).compress()
 	elif op=='-d':
 		o = Dx(fi).decompress()
-	print "The file size was: {} KB".format(os.path.getsize(fi)/1000.0)
-	print "The file size is: {} KB".format(os.path.getsize(o)/1000.0)
+	print "The file size was: {} KB".format(get_size(fi)/1000.0)
+	print "The file size is: {} KB".format(get_size(o)/1000.0)
 
 	
 
