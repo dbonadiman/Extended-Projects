33fc030cf3a0fbdf3b8fe779543ad0e2b6170877
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2013-09-02 19:01:08 +0200

    general refactor whit compression(encoding) class usage changes!

--- Files/Zip File Maker/compression.py
+++ Files/Zip File Maker/compression.py
@@ -1,108 +1,202 @@
-import sys
-import os
-import encoding
-import shutil
+import struct
+
+HUFFMAN = 0
+LZW16 = 1
+LZW32 = 2
+MTF = 3
 
 
-def __compress_file(fi,fo,args):
-	f = open(fi, 'rb')
-	st = f.read()
-	f.close()
-	for arg in args:
-		print(encoding.instance(arg,st))
-		st = encoding.instance(arg,st).encode()
-	out =open(fo, 'wb')
-	out.write(st)
-	out.close()
-	return fo
+class __Huffman(object):
+
+	__input =""
+	__ht = (((((((14, (220, 230)), ((156, 223), 17)), (((191, 201), (235, 250)), ((196, 181), (202, 188)))), ((((164, 208), (229, 175)), ((210, 179), (151, 159))), (((226, 205), (187, 145)), ((251, 186), (153, 124))))), (((((139, 157), (239, 161)), ((246, 137), (168, 207))), ((16, (255, 194)), (12, 19))), ((((213, 155), (141, 158)), ((142, 152), 20)), (((131, 222), (146, 123)), ((195, 218), (171, 206)))))), ((((((169, 160), (163, 134)), ((231, 185), (247, 192))), (((173, 136), (177, 130)), ((190, 118), (128, 167)))), ((((193, 140), (96, 182)), ((172, 148), (126, 203))), (((180, 133), (214, 147)), ((211, 113), (178, 102))))), (((((129, 97), (106, 135)), ((79, 122), (117, 144))), ((10, (165, 127)), ((215, 109), (114, 138)))), (((8, (87, 199)), ((149, 93), (104, 81))), (((92, 94), (132, 86)), ((120, 125), (105, 111))))))), (((((1, ((162, 110), (100, 103))), (((91, 82), 9), ((116, 143), (150, 112)))), ((((64, 108), (174, 68)), (6, (83, 90))), (((107, 121), (88, 119)), ((85, 115), (73, 75))))), (((((95, 72), 7), ((63, 101), 2)), (((80, 77), (74, 66)), ((99, 84), 5))), ((((61, 51), (76, 69)), ((59, 58), (67, 98))), (((70, 71), (89, 78)), ((60, 62), (54, 53)))))), ((((((57, 48), (50, 35)), ((36, 40), 0)), (((55, 37), (47, 52)), ((43, 46), (65, 41)))), ((((45, 34), (38, 49)), ((33, 31), (42, 30))), (((25, 28), (56, 21)), (4, (44, (240, 234)))))), (((((39, 26), (29, (225, 237))), ((22, 24), 3)), (((27, (184, 232)), (32, (233, 242))), ((11, (224, 241)), ((254, 236), (253, 228))))), (((((204, 189), 18), ((197, 249), (219, 245))), (((176, 212), (227, 238)), ((209, 216), 13))), (((15, (154, 217)), ((183, 243), (170, 248))), (((221, 200), (252, 198)), (23, (166, 244)))))))))
+
+	def __init__(self,data):
+		self.__input=data
+		
+	def __chunks(self,l, n):
+		return [l[i:i+n] for i in range(0, len(l), n)]
+		
+	def __bit_to_char(self,bit_str):
+		out = []
+		for b in self.__chunks(bit_str,8):
+			if len(b) < 8:
+				b = b + '0' * (8 - len(b))
+			out+=[int(b, 2)]
+		return out
+
+	def __char_to_bit(self,string):
+		out = ''
+		for c in string:
+			if  isinstance(c, str):
+				c = ord(c)
+			out += '{0:08b}'.format(c)
+		return out
+
+	def __traverse(self,ht,s=None):
+		if s is None:
+			s = ""
+		if not isinstance(ht,tuple):
+			return {ht:s}
+		else:
+			d1 = self.__traverse(ht[0],s+'0')
+			d1.update(self.__traverse(ht[1],s+'1'))
+			return d1
+
+	def encode(self):
+		ht = self.__ht
+		
+		conversion_table = self.__traverse(ht)
+		s = "".join([conversion_table[c] for c in self.__input])
+		b =self.__bit_to_char(s)
+		return bytearray(b)
 
 
-def __decompress_file(fi,fo,args):
-	f = open(fi,'rb')	
-	st = f.read()
-	f.close()
-	for arg in args:
-		print(encoding.instance(arg,st))
-		st = encoding.instance(arg,st).decode()
-	f = open(fo,'wb')
+	def decode(self):
+		#data = self.__loads_data(self.__input)
+		string =bytearray()
+		t = self.__ht
+		for s in self.__char_to_bit(self.__input):
+			if isinstance(t[int(s)], tuple):
+				t = t[int(s)]
+			else:
+				string += bytearray([t[int(s)]])
+				t = self.__ht
+		return string
 
-	f.write(st)
-	f.close
-	return fo
 
-def __folder_pack(fi,fo):
-	if not os.path.isfile(fi):
-		data_to_write=''
-		info = open(fo,'wb')
-		files=os.listdir(fi)
-		info.write('<<FOLDER>>\n'.encode('utf-8'))
-		for fa in files:
-			merged_folder = __folder_pack(fi+"/"+fa,fi+"/"+fa+'.temp')
-			a = open(merged_folder,'rb')
-			data = a.read()
-			a.close()
-			info.write('{}\t{}\n'.format(merged_folder,len(bytearray(data))).encode('utf-8'))
-			info.write(bytearray(data))
-			os.remove(merged_folder)
-		info.write('\n'.encode('utf-8'))
-		info.close()
-	else:
-		shutil.copy(fi,fo)
-	print(fi)		
-	return fo
+class __LZW(object):
 
-def __folder_unpack(fi,fo):
-	info = open(fi,'rb')
-	if info.readline()=='<<FOLDER>>\n'.encode('utf-8'):
-		files = []
-		if not os.path.exists(fo):
-			os.makedirs(fo)
-		while True:
-			s = info.readline().decode('utf-8')
-			if s=='\n':
-				break
-			a = s.split('\t')
-			try:
-				f = (a[0],int(a[1].replace('\n','')))
-			except Exception:
-				raise Exception()
-			data = info.read(f[1])
-			fa = open(f[0].replace('\0',''),'wb')
-			fa.write(data)
-			fa.close()
-			__folder_unpack(f[0].replace('\0',''),f[0][:-5].replace('\0',''))	
-	else:
-		shutil.copy(fi,fo)
-	os.remove(fi)
-	print(fo)
-	return fo
+	__input =""
+	__bit = 16
+	
+	__b = {
+		16: "!H",
+		32: "!I",
+		64: "!Q"
+	}
 
-def compress(fi,fo,pipeline=[encoding.LZW16,encoding.HUFFMAN]):
-	print("Packing...\n")
-	ar = __folder_pack(fi,fi+'.temp')
-	print("Packing...Done\n")
-	print("Compressing...\n")
-	out = __compress_file(ar,fo,pipeline)
-	os.remove(ar)
-	print("Compressing...Done\n")
-	return out
+	def __init__(self,data,bit=16):
+		self.__input=data
+		self.__bit=bit
 
-def decompress(fi,fo,pipeline=[encoding.LZW16,encoding.HUFFMAN]):	
-	print("Decompressing...\n")
-	ar = __decompress_file(fi,fi+'.temp',pipeline[::-1])
-	print("Decompressing...Done\n")
-	print("Unpacking...\n")
-	out = __folder_unpack(ar,fo)
-	print("Unpacking...Done\n")
-	return out
+
+	def encode(self):
+		dict_size = 256
+		#dictionary = dict((chr(i), chr(i)) for i in xrange(dict_size))
+		dic = {str([i]): i for i in range(dict_size)}
+		out_st = bytearray()
+		buff = []
+		for s in bytearray(self.__input):
+			buffers = buff+[s]
+			if str(buffers) in dic:
+				buff = buffers
+			else:
+				out_st+=struct.pack(self.__b[self.__bit], dic[str(buff)])
+				if dict_size<2**self.__bit:
+					dic[str(buffers)] = dict_size
+					dict_size += 1
+				buff = [s]
+		if buff:
+			out_st+=struct.pack(self.__b[self.__bit], dic[str(buff)])
+		return out_st
+
+	def __chunks(self,l, n):
+		return [l[i:i+n] for i in range(0, len(l), n)]
+
+	def decode(self):
+		dict_size = 256
+		#dictionary = dict((chr(i), chr(i)) for i in xrange(dict_size))
+		dic = {i: [i] for i in range(dict_size)}
+		array = [struct.unpack(self.__b[self.__bit], s)[0] for s in self.__chunks(self.__input, int(self.__bit/8))]
+		out_st = pentry = dic[array.pop(0)]
+		for ccode in array:
+			if ccode in dic:
+				entry =  dic[ccode]
+			elif ccode==dict_size:
+				entry = pentry+[pentry[0]]
+			else:
+				raise ValueError(str(ccode))
+			out_st += entry
+			dic[dict_size] = pentry+[entry[0]]
+			dict_size +=1
+			pentry = entry
+		return bytearray(out_st)
+		
+		
+class __MTFT(object):
+
+	def __init__(self,data):
+		self.__input=data
+
+
+	def encode(self):
+		dict_size = 256
+		l = [i for i in range(dict_size)]
+		out_st = []
+		for s in bytearray(self.__input):
+			old_index = l.index(s)
+			l.insert(0, l.pop(old_index))
+			out_st+=[old_index]		
+		return bytearray(out_st)
+
+	def decode(self):
+		dict_size = 256
+		l = [i for i in range(dict_size)]
+		out_st = []
+		for s in bytearray(self.__input):
+			st = l.pop(s)
+			l.insert(0, st)
+			out_st+=[st]		
+		return bytearray(out_st)
+
+
+
+		
+
+def __instance(st,codec):
+	if codec==0:
+		return __Huffman(st)
+	if codec==1:
+		return __LZW(st,16)
+	if codec==2:
+		return __LZW(st,32)
+	if codec==3:
+		return __MTFT(st)
+	print(codec)
+	
+def encode(s,codec):
+	for c in codec:
+		s = instance(s,c).encode()
+	return s
+		
+	
+def decode(s,codec):
+	for c in codec:
+		s = instance(s,c).decode()
+	return s
+
+
+
+
+def __test():
+	import pickle
+	s = open('files/pride_and_prejudice.txt','rb').read()
+	#print 100
+	enc = s
+	enc = encode(s,[LZW16,HUFFMAN])
+	dec = decode(enc,[HUFFMAN,LZW16])
+
+	return dec == s
 
 	
-def __main(op,fi):
-	if op=='-c':
-		compress(fi,fi+'.co')
-	if op=='-d':
-		decompress(fi,fi[:-3])
+
+
+
+
+def __main():
+	print(__test())
 
 
 if __name__=="__main__":
-		__main(sys.argv[1],sys.argv[2])
+	__main()
---
+++ Files/Zip File Maker/compression_main.py
@@ -0,0 +1,122 @@
+import sys
+import os
+import compression 
+import shutil
+
+
+def __compress_file(fi,fo,args):
+	f = open(fi, 'rb')
+	st = f.read()
+	f.close()
+	st = compression.encode(st,args)
+	out =open(fo, 'wb')
+	out.write(st)
+	out.close()
+	return fo
+
+
+def __decompress_file(fi,fo,args):
+	f = open(fi,'rb')	
+	st = f.read()
+	f.close()
+	st = compression.decode(st,args)
+	f = open(fo,'wb')
+
+	f.write(st)
+	f.close
+	return fo
+
+def __folder_pack(fi,fo):
+	if not os.path.isfile(fi):
+		data_to_write=''
+		info = open(fo,'wb')
+		files=os.listdir(fi)
+		info.write('<<FOLDER>>\n'.encode('utf-8'))
+		for fa in files:
+			merged_folder = __folder_pack(fi+"/"+fa,fi+"/"+fa+'.temp')
+			a = open(merged_folder,'rb')
+			data = a.read()
+			a.close()
+			info.write('{}\t{}\n'.format(merged_folder,len(bytearray(data))).encode('utf-8'))
+			info.write(bytearray(data))
+			os.remove(merged_folder)
+		info.write('\n'.encode('utf-8'))
+		info.close()
+	else:
+		shutil.copy(fi,fo)
+	print(fi)		
+	return fo
+
+def __folder_unpack(fi,fo):
+	info = open(fi,'rb')
+	if info.readline()=='<<FOLDER>>\n'.encode('utf-8'):
+		files = []
+		if not os.path.exists(fo):
+			os.makedirs(fo)
+		while True:
+			s = info.readline().decode('utf-8')
+			if s=='\n':
+				break
+			a = s.split('\t')
+			try:
+				f = (a[0],int(a[1].replace('\n','')))
+			except Exception:
+				raise Exception()
+			data = info.read(f[1])
+			fa = open(f[0].replace('\0',''),'wb')
+			fa.write(data)
+			fa.close()
+			__folder_unpack(f[0].replace('\0',''),f[0][:-5].replace('\0',''))	
+	else:
+		shutil.copy(fi,fo)
+	os.remove(fi)
+	print(fo)
+	return fo
+
+def compress(fi,fo,pipeline=None):
+	if pipeline is None:
+		pipeline = [compression.LZW16,compression.HUFFMAN]
+	print("Packing...\n")
+	ar = __folder_pack(fi,fi+'.temp')
+	print("Packing...Done\n")
+	print("Compressing...\n")
+	out = __compress_file(ar,fo,pipeline)
+	os.remove(ar)
+	print("Compressing...Done\n")
+	return out
+
+def decompress(fi,fo,pipeline=None):
+	if pipeline is None:
+		pipeline = [compression.HUFFMAN,compression.LZW16]
+	print("Decompressing...\n")
+	ar = __decompress_file(fi,fi+'.temp',pipeline)
+	print("Decompressing...Done\n")
+	print("Unpacking...\n")
+	out = __folder_unpack(ar,fo)
+	print("Unpacking...Done\n")
+	return out
+
+
+
+def __test():
+	try:
+		compress('files','files.co')
+		decompress('files.co','files')
+	except Exception():
+		print("Failed!!")
+	else:
+		print("Success")
+	
+	
+def __main(op,fi):
+	if op=='-c':
+		compress(fi,fi+'.co')
+	if op=='-d':
+		decompress(fi,fi[:-3])
+
+
+if __name__=="__main__":
+		if len(sys.argv)==3:
+			__main(sys.argv[1],sys.argv[2])
+		else:
+			__test()
