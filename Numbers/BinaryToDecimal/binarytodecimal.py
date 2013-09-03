a44240cfd6043bffbadd29c25bcff34c3cda2390
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2013-09-03 22:19:13 +0200

    bynary to decimal code optimisation PEP-8 style

--- Numbers/BinaryToDecimal/binarytodecimal.py
+++ Numbers/BinaryToDecimal/binarytodecimal.py
@@ -6,14 +6,13 @@ def decimal_to_binary(num):
     return binary[::-1] #this reverse the string
 
 def binary_to_decimal(bin):
-    decimal = 0
-    for i in range(0,len(bin)):
-        if int(bin[i])==1:
-            decimal += (2)**((len(bin)-1)-i)
-    return decimal
+    return sum((2)**((len(bin)-1)-i) if int(bin[i])==1 else 0 for i in range(len(bin)))
 
 def main():
     try:
+        global input
+        try: input = raw_input
+        except NameError: pass
         decimal = int(input("A decimal number: "))
         print (decimal_to_binary(decimal))
         binary = str(input("A binary number: "))
