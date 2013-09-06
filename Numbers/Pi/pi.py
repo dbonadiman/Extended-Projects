6614a721a0b6aeb5f955b72be18b9f6976391c83
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2013-09-06 09:33:12 +0200

    Pi completed, with added tests and docstring, with some comments added!!

--- Numbers/Pi/pi.py
+++ Numbers/Pi/pi.py
@@ -1,29 +1,59 @@
+"""
+Problem
+-------
+**Find PI to the Nth Digit**
+Enter a number and have the program generate PI up to that
+many decimal places. Keep a limit to how far the program
+will go.
 
-###########################
-# More precision is needed.
-# 
-#
-#
-#
-###########################
+Solution
+--------
+I'm gonna use the default pi from the math module
+formatting it using the string formatting function
+(format).
+
+Author
+------
+dbonadiman
+
+"""
+import math
 import sys
 
-def pi():
-    return 2*opi(1,60)
 
-def opi(i,loops):
-    if i==loops:
-        return 1
-    else:
-        n = 1.0 + i / (2.0 * i + 1) * opi(i + 1,loops)
-        return n
+def pi(precision):
+    """
+    Print Pi.
+
+    >>> pi(3)
+    '3.142'
+
+    >>> pi(0)
+    '3'
+
+    >>> pi(50)
+    '3.14159265358979311599796346854418516159057617187500'
+    """
+    outstring = ''.join(['{:.', str(precision), 'f}'])
+    #TODO: find a more precise pi function then math.pi
+    return outstring.format(math.pi)
+
 
 def main():
-    global input
-    try: input = raw_input
-    except NameError: pass
-    outstring = 'The Number is {:.'+str(int(input("Precision:"))+1)+'}'
-    print(outstring.format(pi()))
+    try:
+        print("\nThis program returns the pi up to many"
+              "decimal you enter.\n"
+              "Please enter the number of decimal you need.\n")
+        precision = raw_input("-->") #this only works in 2.* change to input("-->") in 3.*
+        print(''.join(['\n', pi(int(precision)), '\n']))
+        return 0
+    except ValueError:
+        print "Only positive integers allowed!\n"
+        return 1
 
-if __name__=="__main__":
-    main()
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    status = main()
+    sys.exit(status)
