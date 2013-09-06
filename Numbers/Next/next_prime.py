e215c0c7c78040f8d40dbfc070787385d8c3f7a2
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2013-09-06 15:25:58 +0200

    Next prime updated completed, with added tests and docstring, with some comments added!!

--- Numbers/Next/next_prime.py
+++ Numbers/Next/next_prime.py
@@ -1,31 +1,91 @@
-####
-# One is skipped
-#####
+"""
+Problem
+-------
 
-def next(prime):
-    return next_p(prime[len(prime)-1]+1,prime)
+**Next Prime Number**
+Have the program find prime numbers until the user
+chooses to stop asking for the next one.
 
-def next_p(n,prime):
-    for a in prime:
-        if a>=(n/2)+1:
-            return [n]
-        if n%a==0:
-            if n%2==0:
-                return next_p(n+1)
-            else:
-                return next_p(n+2)
-        
+Solution
+--------
+The problem is solved using an iterative and a recursive
+ tecniques
+
+Author
+------
+dbonadiman
+
+"""
+import sys
+
+
+def next_recursive(n=0):
+    """
+    Prime Factorization in an recursive algorithm.
+
+    >>> next_recursive(3)
+    5
+
+    >>> next_recursive(1)
+    2
+
+    >>> next_recursive()
+    1
+    """
+    n += 1
+    # Optimisation:
+    # we try to divide only by 2 and the
+    # odds number between 3 and the
+    # candidate div 2
+    div = range(3, int(n/2), 2)
+    if n != 2:
+        div.append(2)
+    for a in div:
+        if n % a == 0:
+            return next_recursive(n)
+    return n
+
+
+def next_iterative(n=0):
+    """
+    Prime Factorization in an recursive algorithm.
+
+    >>> next_iterative(3)
+    5
+
+    >>> next_iterative(1)
+    2
+
+    >>> next_iterative()
+    1
+    """
+    while True:
+        n += 1
+        div = range(3, int(n/2), 2)
+        if n != 2:
+            div.append(2)
+        divisible = False
+        for a in div:
+            divisible = divisible or (n % a == 0)
+        if not divisible:
+            break
+    return n
+
+
 def main():
-    print(2)
-    prime = [2]
-    while input()!='n':
-        n = next(prime)
-        print(n[0])
-        prime += n
+    n_r = next_recursive()
+    n_i = next_recursive()
+    while not raw_input("Press enter to get another prime number! "
+                        "(q to quit)").lower().startswith('q'):
+        print("Recursive: {}".format(n_r))
+        n_r = next_recursive(n_r)
+        print("Iterative: {}".format(n_i))
+        n_i = next_recursive(n_i)
+    return 0
 
-if __name__=="__main__":
-    main()
 
-
-
-        
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    status = main()
+    sys.exit(status)
