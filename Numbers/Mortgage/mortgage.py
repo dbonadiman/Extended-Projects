e77ee3f367c58f74cebd231771b2d7dce3f2b678
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2013-09-06 17:01:08 +0200

    Mortgage updated completed, with added tests and docstring, with some comments added!!

--- Numbers/Mortgage/mortgage.py
+++ Numbers/Mortgage/mortgage.py
@@ -1,18 +1,73 @@
+"""
+Problem
+-------
+**Mortgage Calculator**
+
+Calculate the monthly payments of a fixed
+term mortgage over given Nth terms at a given
+interest rate. Also figure out how long it
+will take the user to pay back the loan.
+
+
+Solution
+--------
+A function to calculate the montly payments and
+one to find the total amount to pay
+
+Author
+------
+dbonadiman
+
+"""
 import sys
 
-def montly_amount(mortgage,months,interest_rate):
-    return ((mortgage*(interest_rate/12)*(1+interest_rate/12)**months))/(((1+interest_rate/12)**months)-1)
+
+def total_mortgage(montly_amount, months):
+    """
+    Calculate the total amounts given
+    the montly amount and the number of
+    months
+
+    >>> total_mortgage(1000,12)
+    12000
+    """
+    return montly_amount*months
+
+
+def montly_amount(mortgage, months, interest_rate):
+    """
+    Calculate the montly amount to pay given
+    the mortgage amount, the annual interest rate
+    and the number of months
+
+    M*C*(1+C)^N
+    -----------
+    ((1+C)^N)-1
+
+    >>> montly_amount(100,12,0.1)
+    8.79
+    """
+    montly_interest = interest_rate/12
+    num = ((mortgage*(montly_interest)*(1+montly_interest)**months))
+    den = (((1+montly_interest)**months)-1)
+    return round(num/den, 2)
+
 
 def main():
-    global input
-    try: input = raw_input
-    except NameError: pass
-    mortgage = float(input("Mortgage amount?"))
-    interest_rate = float(input("Interest rate?"))
-    terms  = float(input("Terms(in month)"))
-    print("montly fee {:.2f}".format(montly_amount(mortgage,terms,interest_rate)))
-    print("Total {:.2f} ".format(montly_amount(mortgage,terms,interest_rate)*terms))
+    print("\nThis program calculate how much do you have to pay\n"
+          "to pay back a Mortgage given the number of month and\n"
+          "the interest rate\n")
+    mortgage = float(raw_input("Mortgage amount? "))
+    interest_rate = float(raw_input("Interest rate? "))
+    terms = float(raw_input("Terms(in month) "))
+    montly = montly_amount(mortgage, terms, interest_rate)
+    print("Montly fee {:.2f}".format(montly))
+    print("Total {:.2f} ".format(total_mortgage(montly, terms)))
     return 0
 
-if __name__=="__main__":   
-    sys.exit(main())
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    status = main()
+    sys.exit(status)
