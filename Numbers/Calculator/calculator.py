c492c36d060b6bed7db11b415695ad833f9764e9
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2013-09-07 00:00:37 +0200

    calculator updated updated completed, with added tests and docstring, with some comments added!!

--- Numbers/Calculator/calculator.py
+++ Numbers/Calculator/calculator.py
@@ -1,24 +1,74 @@
-def operation(n1,n2,op):
-    return {
-        '+':n1+n2,
-        '-':n1-n2,
-        '*':n1*n2,
-        '/':n1/n2
-    }.get(op,"Wrong operator")
+"""
+Problem
+-------
+**Calculator**
+A simple calculator to do basic operators.
+Make it a scientific calculator for added complexity.
+
+
+Solution
+--------
+This program executes basic operation between float
+
+Author
+------
+dbonadiman
+
+"""
+import sys
+
+
+def operation(n1, n2, op):
+    """
+    Converts a binary string to a real number!
+
+    >>> operation(3, 2, '+')
+    5
+
+    >>> operation(3, 2, '-')
+    1
+
+    >>> operation(3, 2, '*')
+    6
+
+    >>> operation(3.0, 2.0, '/')
+    1.5
+
+    >>> operation(3.0, 2.0, '^')
+    Traceback (most recent call last):
+        ...
+    ValueError: Wrong operator only + - * / allowed
+
+    """
+    if op not in "+-*/":
+        raise ValueError("Wrong operator only + - * / allowed")
+    dic = {
+        '+': n1+n2,
+        '-': n1-n2,
+        '*': n1*n2,
+        '/': n1/n2
+        }
+    return dic.get(op)
+
 
 def main():
     try:
-        global input
-        try: input = raw_input
-        except NameError: pass
-        n1 = int(input("Number 1: "))
-        n2 = int(input("Number 2: "))
-    except Exception:
-        print("Wrong input, retry.")
-        main()
-    else:
-        operator = str(input("Operator(+,-,*,/)"))
-        print(operation(n1,n2,operator))
+        print("\nThis program converts decimal number to binary"
+              "and vice-versa.\n")
 
-if __name__=="__main__":
-    main()
+        n1 = float(raw_input("Number 1: "))
+        n2 = float(raw_input("Number 2: "))
+        operator = str(raw_input("Operator (+, -, *, /): "))
+        print("\n{} {} {} = {}".format(n1, operator,
+                                       n2, operation(n1, n2, operator)))
+        return 0
+    except Exception, e:
+        print(e)
+        return 1
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    status = main()
+    sys.exit(status)
