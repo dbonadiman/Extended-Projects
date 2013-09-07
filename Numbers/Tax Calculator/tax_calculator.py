5d12138a84971bd2c529b6808051fd8318b78081
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2013-09-07 12:13:40 +0200

    Tax Calculator updated updated completed, with added tests and docstring, with some comments added!!

--- Numbers/Tax Calculator/tax_calculator.py
+++ Numbers/Tax Calculator/tax_calculator.py
@@ -1,16 +1,75 @@
+"""
+Problem
+-------
+**Tax Calculator**
+Asks the user to enter a cost
+and either a country or state tax.
+It then returns the tax plus
+the total cost with tax.
+
+
+Solution
+--------
+a simple function that calculates the
+tax to pay
+
+
+Author
+------
+dbonadiman
+
+"""
 import sys
 
-def _setup():
-    global input
-    try: input = raw_input
-    except NameError: pass
+
+def tax_calculator(tax, cost):
+    """
+    Tax_calculator
+    07 Sep 2013
+
+    This function is used to calculate tax
+    plus the the total cost plus tax
+
+    Parameter:
+
+     tax => must be a float in the form 0.21 to
+            represent a 21'%' tax
+
+     cost => the cost of the goods to buy
+
+    Test:
+
+    >>> tax_calculator(0.1, 100)
+    10.0
+
+    >>> tax_calculator(0, 100)
+    0.0
+
+    >>> tax_calculator(1.0, 100)
+    100.0
+    """
+    return float(tax * cost)
+
 
 def main():
-    tax_amount = float(input("Tax --> "))
-    cost = float(input("Cost --> "))
-    print("You will pay: {:.2f} ".format(tax_amount*cost+cost))
+    try:
+        print("\nThis program calculate amount you have to pay given\n"
+              "the tax of your state and the cost of the good\n")
 
-if __name__=="__main__":
-    _setup()
+        print("Please enter the Tax to pay (0.21 not 21%)")
+        tax = float(raw_input("--> "))
+        print("Please enter the Cost of the good")
+        cost = float(raw_input("--> "))
+
+        print("You will pay: {:.2f} ".format(tax_calculator(tax, cost)+cost))
+        return 0
+    except Exception, e:
+        print(e)
+        return 1
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
     status = main()
     sys.exit(status)
