c15ad05d12f04e0de686b64a9f76187886d1d5c5
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2013-09-07 10:24:04 +0200

    Alarm updated updated completed, with added tests and docstring, with some comments added!!

--- Numbers/Alarm Clock/alarm.py
+++ Numbers/Alarm Clock/alarm.py
@@ -1,24 +1,49 @@
+"""
+Problem
+-------
+**Alarm Clock**
+A simple clock where it plays
+a sound after X number of minutes/seconds
+or at a particular time.
+
+
+Solution
+--------
+
+This solution is provided using the "bip"
+of the console
+
+
+Author
+------
+dbonadiman
+
+"""
+
 import sys
 import time
 
-
+#TODO: find a better way to play the sound
 def alarm(n):
     for i in range(n):
         print('\a')
         time.sleep(0.2)
     print()
-        
+
 
 def main():
-    global input
-    try: input = raw_input
-    except NameError: pass
-    seconds = int(input("seconds:"))
-    time.sleep(seconds)
-    alarm(20)
-    
-    return 0
+    try:
+        seconds = int(raw_input("seconds:"))
+        time.sleep(seconds)
+        alarm(20)
+        return 0
+    except Exception, e:
+        print(e)
+        return 1
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
     status = main()
     sys.exit(status)
