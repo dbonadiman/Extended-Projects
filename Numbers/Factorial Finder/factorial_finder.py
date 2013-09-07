fb04cd2ba64699b0104947aad3755c1ab0c33a70
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2013-09-07 13:47:38 +0200

    Factorial Finder Updated
    Added:
    Documentation
    Test
    PEP-8 validated

--- Numbers/Factorial Finder/factorial_finder.py
+++ Numbers/Factorial Finder/factorial_finder.py
@@ -1,26 +1,90 @@
-def factorial(n):
+"""
+Problem
+-------
+**Factorial Finder**
+The Factorial of a positive integer,
+n, is defined as the product of the sequence
+n, n-1, n-2, ...1 and the factorial of zero,
+0, is defined as being 1. Solve this using
+both loops and recursion.
+
+
+Solution
+--------
+A solution is presented in both iterative and
+recursive way :)
+
+
+Author
+------
+dbonadiman
+
+"""
+import sys
+
+
+def factorial_iterative(n):
+    """
+    factorial_iterative(n)
+
+    This function computes the factorial
+    of a number n in an iterative way
+
+    Parameters:
+
+    n ==> the integer used for to compute N!
+
+    Tests:
+
+    >>> factorial_iterative(5)
+    120
+
+    >>> factorial_iterative(0)
+    1
+
+    """
     factorial = 1
     for i in range(n):
-        factorial*=i+1
+        factorial *= i+1
     return factorial
 
-def factorial_rec(n):
-        return n*factorial_rec(n-1) if n>1 else 1
+
+def factorial_recursive(n):
+    """
+    factorial_recursive(n)
+
+    This function computes the factorial
+    of a number n in an recursive way
+
+    Parameters:
+
+    n ==> the integer used for to compute N!
+
+    Tests:
+
+    >>> factorial_recursive(5)
+    120
+
+    >>> factorial_recursive(0)
+    1
+
+    """
+    return n * factorial_recursive(n-1) if n else 1
+
 
 def main():
     try:
-        global input
-        try: input = raw_input
-        except NameError: pass
-        n = int(input("N: "))
-    except Exception:
-        print ("Wrong input,retry.")
-    else:
-        print ("N! iterative : {}".format(factorial(n)))
-        try:
-            print ("N! recoursive : {}".format(factorial_rec(n)))
-        except Exception:
-            print("Too much recursion")
-        
-if __name__=="__main__":
-    main()
+        n = int(raw_input("-->"))
+        print ("N! iterative : {}".format(factorial_iterative(n)))
+        print ("N! recoursive : {}".format(factorial_recursive(n)))
+        return 0
+    except Exception, e:
+        print(e)
+        return 1
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    status = main()
+    sys.exit(status)
