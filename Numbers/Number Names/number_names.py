9d8d9855a95e1d4c5a7516b993fc13ac43e1c206
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2013-09-08 14:58:36 +0200

    Number Names Updated
    Documentation Added.
    Test Added

--- Numbers/Number Names/number_names.py
+++ Numbers/Number Names/number_names.py
@@ -1,77 +1,128 @@
-_onetotwenty = {
-    1:"one",
-    2:"two",
-    3:"three",
-    4:"four",
-    5:"five",
-    6:"six",
-    7:"seven",
-    8:"eight",
-    9:"nine",
-    10:"ten",
-    11:"eleven",
-    12:"twelve",
-    13:"thirteen",
-    14:"fourteen",
-    15:"fiveteen",
-    16:"sixteen",
-    17:"seventeen",
-    18:"eighteen",
-    19:"nineteen"
-}
+"""
+Problem
+-------
+**Number Names**
+Show how to spell out a number in English.
+You can use a preexisting implementation
+or roll your own, but you should support
+inputs up to at least one million (or the maximum
+value of your language's default bounded integer
+type, if that's less). *Optional: Support for
+inputs other than positive integers
+(like zero, negative integers, and
+floating-point numbers).*
 
-_dec = {
-    20:"twenty",
-    30:"thirty",
-    40:"forty",
-    50:"fifty",
-    60:"sixty",
-    70:"seventy",
-    80:"eighty",
-    90:"ninety"
-}
 
-_uptotrillion = {
-    1000000000000:"trillion",
-    1000000000:"billion",
-    1000000:"million",
-    1000:"thousand",
-    100:"hundred"        
-}
+Solution
+--------
+The solution is provided in a recursive way.
+Using a dictionary mapping the number with the
+related name
+
+Author
+------
+dbonadiman
+
+"""
+import sys
+
 
 def digit_to_name(n):
-    for dig in _uptotrillion:
-        if int(n/dig)!=0:
-            if n%dig == 0:
-                return digit_to_name(int(n/dig))+" "+_uptotrillion[dig]
+    """
+    digit_to_name
+
+    This recursive function takes in input a number
+    and outputs the name of the number in english
+
+    Parameters:
+    n ==> The number in input(it must be and integer)
+
+    Test:
+
+    >>> digit_to_name(3)
+    'three'
+
+    >>> digit_to_name(33)
+    'thirty-three'
+    """
+    onetotwenty = {
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+        10: "ten",
+        11: "eleven",
+        12: "twelve",
+        13: "thirteen",
+        14: "fourteen",
+        15: "fiveteen",
+        16: "sixteen",
+        17: "seventeen",
+        18: "eighteen",
+        19: "nineteen"
+    }
+
+    dec = {
+        20: "twenty",
+        30: "thirty",
+        40: "forty",
+        50: "fifty",
+        60: "sixty",
+        70: "seventy",
+        80: "eighty",
+        90: "ninety"
+    }
+
+    uptotrillion = {
+        1000000000000: "trillion",
+        1000000000: "billion",
+        1000000: "million",
+        1000: "thousand",
+        100: "hundred"
+    }
+
+    for dig in uptotrillion:
+        if int(n/dig) != 0:
+            if n % dig == 0:
+                return "".join([digit_to_name(int(n / dig)),
+                                " ", uptotrillion[dig]])
             else:
-                return digit_to_name(int(n/dig))+" "+_uptotrillion[dig]+" "+digit_to_name(n%dig)
+                return "".join([digit_to_name(int(n / dig)),
+                                " ", uptotrillion[dig],
+                                " ", digit_to_name(n % dig)])
 
-    if n<100 and n>=20:
-        for dig in _dec:
-            if n-dig<10 and n-dig>0:
-                return _dec[dig]+"-"+digit_to_name(n-dig)
-            elif n-dig==0:
-                return _dec[dig]
+    if n < 100 and n >= 20:
+        for dig in dec:
+            if n-dig < 10 and n-dig > 0:
+                return "".join([dec[dig], "-", digit_to_name(n-dig)])
+            elif not n-dig:
+                return dec[dig]
 
-    return _onetotwenty[n]
+    return onetotwenty[n]
+
 
 def main():
     try:
-        global input
-        try: input = raw_input
-        except NameError: pass
-        n = int(input("N: "))
-        if n==0:
-            raise Exception('error','wrong input')
-    except Exception:
-        print("Wrong input, retry.")
-        main()
-    else:
+        print("\nThis program takes in input an integer and outputs\n"
+              "the number's name in english\n"
+              "Please enter the number:\n")
+        n = int(raw_input("-->"))
+        if not n:
+            raise ValueError("Only numbers greather than 0 allowed")
         print(digit_to_name(n))
+        return 0
+    except Exception, e:
+        print(e)
+        return 1
 
-if __name__=="__main__":
-    main()
 
-
-
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    status = main()
+    sys.exit(status)
