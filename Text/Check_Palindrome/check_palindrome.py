1ea61a05517e442f130d1440ed4e3d0f963753bc
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2013-09-11 11:14:40 +0200

    Palindrome Updated
    Added Documentation
    Added Tests
    Solution simplified

--- Text/Check_Palindrome/check_palindrome.py
+++ Text/Check_Palindrome/check_palindrome.py
@@ -1,20 +1,61 @@
+"""
+Problem
+-------
+**Check if Palindrome**
+Checks if the string entered by the user
+is a palindrome. That is that it reads
+the same forwards as backwards like racecar
+
+
+Solution
+--------
+The solution is provided simply checking if the string
+is equal to the same string reversed.
+
+
+
+Author
+------
+dbonadiman
+
+"""
+import sys
+
+
 def palindrome(s):
-    for i in range(0,len(s)):
-        if s[i]!=s[(len(s)-1)-i]:
-            return False
-    return True
+    """
+    palindrome
+    This function simply checks if the string
+    is equals to itself reversed
+
+    Parameters:
+    s ==> the string
+
+    Test:
+    >>> palindrome('racecar')
+    True
+
+    >>> palindrome('ciao')
+    False
+    """
+    return s == s[::-1]
+
 
 def main():
     try:
-        global input
-        try: input = raw_input
-        except NameError: pass
-        string = str(input(""))
-    except Exception:
-        print("Wrong input, retry.")
-        main()
-    else:
+        print("\nThis program checks if a sting is palindrome\n"
+              "Please enter the string you want to check: \n")
+        string = raw_input("--> ")
+        print('')
         print(palindrome(string))
+        return 0
+    except Exception, e:
+        print(e)
+        return 1
 
-if __name__=="__main__":
-    main()
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    status = main()
+    sys.exit(status)
