8c053209b743011a560faa34f0a6846cf08083a6
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2013-09-11 11:35:40 +0200

    Count Word Updated
    Added Documentation
    Added Tests

--- Text/Count_Words/count_words.py
+++ Text/Count_Words/count_words.py
@@ -1,17 +1,61 @@
+
+
+"""
+Problem
+-------
+**Count Words in a String**
+Counts the number of individual words in a string.
+For added complexity read these strings in from a
+text file and generate a summary.
+
+
+Solution
+--------
+The solution is provided using a simple string split
+
+Author
+------
+dbonadiman
+
+"""
+import sys
+
+
 def words(s):
-    return len([word for word in s.split(" ") if word not in ".,?!"] )
+    """
+    words
+    This function simpli split the string in spaces
+    and counts the number of splits, it's a very
+    simple and sometimes wrong.
+
+    Parameters:
+    s ==> the string from wich to count the vowels
+
+    Test:
+
+    >>> words('ciao come va')
+    3
+
+    >>> words('cat')
+    1
+    """
+    return len(s.split(" "))
+
 
 def main():
     try:
-        global input
-        try: input = raw_input
-        except NameError: pass
-        text = input("")
-    except Exception:
-        print("Wrong input,retry.")
-        main()
-    else:
-        print(words(text))
+        print("\nThis program counts the words in a string\n"
+              "Please enter the string: \n")
+        string = raw_input("--> ")
+        print(words(string))
+        return 0
+    except Exception, e:
+        print(e)
+        return 1
 
-if __name__=="__main__":
-    main()
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    status = main()
+    sys.exit(status)
