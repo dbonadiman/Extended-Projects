f298f9b7f13b0f1933702b68fdb0ce2294580fcf
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2013-09-11 10:02:36 +0200

    Pig Latin Updated
    Added Description
    Added Documentation
    Added Test

--- Text/Pig_Latin/pig_latin.py
+++ Text/Pig_Latin/pig_latin.py
@@ -1,17 +1,67 @@
+"""
+Problem
+-------
+**Pig Latin**
+Pig Latin is a game of alterations played
+on the English language game. To create the
+Pig Latin form of an English word the initial
+consonant sound is transposed to the end of the
+word and an ay is affixed (Ex.: "banana" would
+yield anana-bay). Read Wikipedia for more
+information on rules.
+
+
+Solution
+--------
+The solution is done doing a list split and then a string
+join with some pig latins added ;)
+
+
+Author
+------
+dbonadiman
+
+"""
+import sys
+
+
 def pig_latin(s):
-    return s[1:]+"-"+s[0]+"ay"
+    """
+    pig_latin
+    It uses the string join function
+    to create a list
+
+    Parameter:
+    s ==> the string to piggify
+
+    Test:
+    >>> pig_latin('ciao')
+    'iao-cay'
+
+    >>> pig_latin('banana')
+    'anana-bay'
+    """
+    return "".join([s[1:], "-", s[0], "ay"])
+
 
 def main():
     try:
-        global input
-        try: input = raw_input
-        except NameError: pass
-        string = input("")
-    except Exception:
-        print("Wrong input,retry.")
-        main()
-    else:
-        print(pig_latin(string))
+        print("\nThis program piggify a string\n"
+              "More in detail it takes your string and makes and\n"
+              "Translate it in pig latin removing the first letter and\n"
+              "adding it to the end folowed by ay.\n"
+              "Please enter the string you want to convert.\n")
 
-if __name__=="__main__":
-    main()
+        string = raw_input("--> ")
+        print(pig_latin(string))
+        return 0
+    except Exception, e:
+        print(e)
+        return 1
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    status = main()
+    sys.exit(status)
