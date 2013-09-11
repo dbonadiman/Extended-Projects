8ec2b1edff65906eaf875841cf9636edff42553f
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2013-09-11 09:08:08 +0200

    Reverse string updated
    First commit of the day
    
    Added Documentation
    Added tests

--- Text/Reverse_String/reverse_string.py
+++ Text/Reverse_String/reverse_string.py
@@ -1,17 +1,67 @@
+"""
+Problem
+-------
+**Reverse a String**
+Enter a string and the program
+will reverse it and print it out.
+
+
+Solution
+--------
+This problem is quite simple and
+can be solved using the string as a list
+and reverse it.
+For visual way i kept a separate functions
+but it's not necessary cause what you need
+to reverse a string is about 6 character long
+
+Author
+------
+dbonadiman
+
+"""
+import sys
+
+
 def reverse(s):
+    """
+    reverse
+    This function reverse a string it uses the same
+    approach used to manipulate lists
+
+    Parameters:
+
+    s==> the string to reverse
+
+    Test:
+
+    >>> reverse('ciao')
+    'oaic'
+
+    >>> reverse('oil')
+    'lio'
+
+    >>> reverse('')
+    ''
+
+    """
     return s[::-1]
+
 
 def main():
     try:
-        global input
-        try: input = raw_input
-        except NameError: pass
-        string = input("")
-    except Exception:
-        print("Wrong input, retry.")
-        main()
-    else:
+        print("\nThis program reverse a string\n"
+              "please enter the string you want to reverse: \n")
+        string = raw_input("-->")
         print(reverse(string))
+        return 0
+    except Exception, e:
+        print(e)
+        return 1
 
-if __name__=="__main__":
-    main()
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    status = main()
+    sys.exit(status)
