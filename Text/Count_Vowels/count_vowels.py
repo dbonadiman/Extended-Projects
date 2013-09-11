48fe310aa2e9be68392cb3876e94a004ebbe8639
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2013-09-11 10:37:04 +0200

    Count Vowels Updated
    Added Documentation
    Added Tests
    
    Optimised the lambda function.

--- Text/Count_Vowels/count_vowels.py
+++ Text/Count_Vowels/count_vowels.py
@@ -1,18 +1,80 @@
+"""
+Problem
+-------
+**Count Vowels**
+Enter a string and the program counts
+the number of vowels in the text.
+For added complexity have it report a
+sum of each vowel found.
+
+
+Solution
+--------
+The solution is provided using a lambda function
+
+Author
+------
+dbonadiman
+
+"""
+import sys
+
+
 def vowels(s):
-    i = lambda s: i(s[1:])+1 if len(s)>0 and s[0] in "aeiou" else i(s[1:]) if len(s)>0 else 0
+    """
+    vowels
+    This solution uses a lambda function that
+    sums one if the first letter of the word is a vowel,
+    zero othrerwise and then recursively on the rest of the word.
+
+    There is a funny optimisation using the fact that in
+    python boolean and integer are actually the same thing
+    so we actually sum the condition instead of extensively
+    write
+
+    if condition:
+        return A+1
+    else:
+        return A
+
+    we use this compact form
+
+    return A+condition
+
+    Parameters:
+    s ==> the string from wich to count the vowels
+
+    Test:
+
+    >>> vowels('ciao')
+    3
+
+    >>> vowels('cat')
+    1
+
+    >>> vowels('mmm')
+    0
+    """
+
+    #Python is fuking awesome
+    i = lambda s: i(s[1:])+(s[0] in "aeiou") if len(s) else 0
     return i(s)
+
 
 def main():
     try:
-        global input
-        try: input = raw_input
-        except NameError: pass
-        string = input("")
-    except Exception:
-        print("Wrong input,retry.")
-        main()
-    else:
+        print("\nThis program counts the vowels in a string\n"
+              "Please enter the string: \n")
+        string = raw_input("--> ")
         print(vowels(string))
+        return 0
+    except Exception, e:
+        print(e)
+        return 1
 
-if __name__=="__main__":
-    main()
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    status = main()
+    sys.exit(status)
