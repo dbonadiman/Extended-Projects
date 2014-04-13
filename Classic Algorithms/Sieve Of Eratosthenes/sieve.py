afeb6ab02c49da9dd015ffb9ffa508cfa2947509
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2014-04-13 18:08:34 +0200

    sieve of erathostene changed

--- Classic Algorithms/Sieve Of Eratosthenes/sieve.py
+++ Classic Algorithms/Sieve Of Eratosthenes/sieve.py
@@ -28,6 +28,7 @@ dbonadiman
 
 """
 import sys
+from array import array
 
 
 def sieve(n):
@@ -40,18 +41,8 @@ def sieve(n):
     Parameters:
     n ==> n - 1 is the intervall in which the algorithm search
           for the prime numer
-
-    Test:
-    >>> sieve(10)
-    [1, 2, 3, 5, 7]
-
-    >>> sieve(0)
-    []
-
-    >>> sieve(20)
-    [1, 2, 3, 5, 7, 11, 13, 17, 19]
     """
-    def primen(a, n, l):
+    def primen(a, n):
         """
         primen
 
@@ -61,56 +52,43 @@ def sieve(n):
         avoinding all the multiple of 2 and
         3 this speeds up a lot the sieve
         having a lot less number to process.
-
-        >>> list(primen(5, 20, 1))
-        [5, 7, 11, 13, 17, 19]
         """
         num = a
         w = [2, 4]
-        nn = len(w)
         i = 0
         while num <= n:
             yield num
-            num += w[i]*l
-            i = (i+1) % nn
+            num += w[i]
+            i = (i+1) % 2
 
-    if n < 3:
-        return []
-    temp = [1]*n
-    temp[0] = temp[1] = 0
-    prime = [1, 2, 3]
-    hop = 1
-    for i in primen(5, n, 1):
-        if temp[i]:
-            prime.append(i)
-            if hop:
-                hop = 0
-                for j in range(i*i, n, i*2):
-                    if not j % i:
-                        hop += 1
-                        temp[j] = 0
-    return prime
+    if n > 3:
+        temp = array('H', [1])*n
+        temp[0] = temp[1] = 0
+        for i in range(1, 4):
+            yield i
+        hop = 1
+        for i in primen(5, n):
+            if temp[i]:
+                if hop:
+                    hop = 0
+                    for j in range(i*i, n, i*2):
+                        if not j % i:
+                            hop = 1
+                            temp[j] = 0
+                yield i
 
 
 def main():
     try:
-        print("\nThis program execute the sieve of Eratosthenes\n"
-              "between 0 and 10000000 and outputs the number of\n"
-              "prime numbers found, please be patient it will\n"
-              "took some time\n")
-
         #here we print only the lenght of the list
         #for visual constraint
         print("The nuber of prime numbers"
-              " under 10000000 is {}".format(len(sieve(10000000))))
+              " under 10000000 is {}".format(len(list(sieve(10000000)))))
         return 0
-    except Exception, e:
-        print(e)
+    except Exception:
         return 1
 
 
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
     status = main()
     sys.exit(status)
